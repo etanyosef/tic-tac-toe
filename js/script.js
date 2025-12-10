32b740dc826ae731269f32bcb3ddcda4a162fdeb
Ethan <ethancredo.ascc@outlook.com>
2025-12-10 14:27:28 +0800

    draw logic board

---
+++ js/script.js
@@ -0,0 +1,87 @@
+
+function gameBoard() {
+    const rows = 3;
+    const columns = 3;
+    const board = [];
+
+    for (let i = 0; i < rows; i++) {
+        board[i] = [];
+        for (let j = 0; j < columns; j++) {
+            board[i].push(cell());
+        }
+    }
+
+    const getBoard = () => board;
+
+    const placeToken = (cell, player) => {
+        const availableCells = board.filter( (row) => row[cell].getValue() === 0 );
+
+        if ( !(availableCells === 0) ) return;
+
+        board[cell].addToken(player);
+    }
+
+    const printBoard = () => {
+        const boardWithCellValues = board.map( (row) => row.map( (cell)  => cell.getValue() ) );
+        console.log(boardWithCellValues);
+    }
+
+    return {
+        getBoard, placeToken, printBoard
+    }
+}
+
+function cell() {
+    let value = 0;
+
+    const addToken = (player) => {
+        value = player;
+    }
+
+    const getValue = () => value;
+
+    return { addToken, getValue };
+}
+
+function gameController(playerOne = 'p1', playerTwo = 'p2') {
+    const board = gameBoard();
+
+    const players = [
+        {
+            name: playerOne,
+            token: 1
+        },
+        {
+            name: playerTwo,
+            token: 2
+        }
+    ];
+
+    let activePlayer = players[0];
+
+    const switchPlayerTurn = () => {
+        activePlayer = activePlayer === players[0]? players[1] : players[0];
+    }
+
+    const getActivePlayer = () => activePlayer;
+
+    const printNewRound = () => {
+        board.printBoard();
+        console.log(`${getActivePlayer().name}'s turn...`);
+    }
+
+    const playRound = (cell) => {
+        console.log(`Placing ${getActivePlayer().name}'s token into cell ${cell}`);
+
+        board.placeToken(cell, getActivePlayer().token);
+
+        switchPlayerTurn();
+        printNewRound();
+    }
+
+    printNewRound();
+
+    return { playRound, getActivePlayer, getBoard: board.getBoard }
+}
+
+gameController();
