8e6b67fd7931d19894b43a73ed7c7d27552ec18d
Ethan Joseph <ethancredo.ascc@outlook.com>
2025-12-11 22:02:01 +0800

    create createPlayer function factory

--- js/script.js
+++ js/script.js
@@ -7,20 +7,7 @@ function displayController() {
 
 }
 
-const createPlayer = () => {
-    const players = [];
-
-    function player(playerName, playerToken) {
-        this.name = playerName,
-        this.token = playerToken
-    }
-
-    const playerOne = new player('Sagi', 'X');
-    const playerTwo = new player('Mimoy', 'O');
-    players.push(playerOne, playerTwo);
-
-    console.table(players);
-
-    return { players, player }
+function createPlayer(name, token) {
+    return { name, token }
 }
 
